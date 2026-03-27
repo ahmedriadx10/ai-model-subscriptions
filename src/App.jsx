3319248b171c9cbf7145d910a183f420ac3c526b
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-03-27 22:28:12 +0600

    ai model subscriptions basic setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-center text-primary text-4xl py-2c">Hello I am from App component</h1>
+    </div>
+  );
+};
+
+export default App;
